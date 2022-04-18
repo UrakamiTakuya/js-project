9493bf1a75daa5530c50e9a85ef78e326ca41a2b
UrakamiTakuya <urakami_t@mediaxis.jp>
2022-04-18 14:04:28 +0900

    回答したら次の問題を表示し、正答数を表示する

--- js-quiz/app.js
+++ js-quiz/app.js
@@ -1,30 +1,57 @@
-const question = 'ゲーム市場、最も売れたゲーム機は次の内どれ？'
-const answers = [
-    'スーパファミコン2',
-    'プレイステーション2',
-    '任天堂スイッチ',
-    '任天堂DS'
-];
-const correct = '任天堂DS';
+const quiz = [
+    {
+        question: 'ゲーム市場、最も売れたゲーム機は次の内どれ？',
+        answers: [
+            'スーパファミコン2',
+            'プレイステーション2',
+            '任天堂スイッチ',
+            '任天堂DS'
+        ],
+        correct: '任天堂DS'
+    }, {
+        question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+        answers: [
+            'MOTHER2',
+            'スーパーマリオブラザーズ2',
+            'スーパードンキーコング',
+            '星のカービィ'
+        ],
+        correct: 'MOTHER2'
+    }
+]
+let quizIndex = 0;
+const quizLength = quiz.length;
+let score = 0;
 
 // 「$」でHTMLのオブジェクトが入っていることが理解しやすい
 const $button = document.getElementsByTagName('button');
 
 // クイズの問題文、選択肢を定義
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex]['question'];
     for (let i = 0; i < 4; i ++) {
-        $button[i].textContent = answers[i];
+        $button[i].textContent = quiz[quizIndex].answers[i];
     }
 }
 setupQuiz();
 
 // ボタンをクリックしたら正誤判定
 const clickHandler = (e) => {
-    if(correct === e.target.textContent) {
+    if(quiz[quizIndex].correct === e.target.textContent) {
         window.alert('正解！');
+        score++;
     } else {
         window.alert('不正解！');
+    }
+
+    quizIndex++;
+
+    if (quizIndex < quizLength) {
+        // 問題文があればこちらを実行
+        setupQuiz()
+    }else {
+        window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です');
+
     }
 }
 
@@ -33,3 +60,4 @@ for(let i = 0; i < 4; i ++) {
         clickHandler(e);
     });
 }
+
