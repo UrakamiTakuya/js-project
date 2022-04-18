2c3714a9d4bf9255172af372aee798ae2f71e66b
UrakamiTakuya <urakami_t@mediaxis.jp>
2022-04-18 13:41:46 +0900

    問題文表示、ボタンをクリックしたら正誤判定のリファクタリング

--- js-quiz/app.js
+++ js-quiz/app.js
@@ -7,41 +7,29 @@ const answers = [
 ];
 const correct = '任天堂DS';
 
-document.getElementById('js-question').textContent = question;
-
 // 「$」でHTMLのオブジェクトが入っていることが理解しやすい
 const $button = document.getElementsByTagName('button');
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3]; 
+// クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = question;
+    for (let i = 0; i < 4; i ++) {
+        $button[i].textContent = answers[i];
+    }
+}
+setupQuiz();
 
-document.getElementsByTagName('button')[0].addEventListener('click', () => {
-    if(correct === $button[0].textContent) {
+// ボタンをクリックしたら正誤判定
+const clickHandler = (e) => {
+    if(correct === e.target.textContent) {
         window.alert('正解！');
     } else {
         window.alert('不正解！');
     }
-});
-document.getElementsByTagName('button')[1].addEventListener('click', () => {
-    if(correct === $button[1].textContent) {
-        window.alert('正解！');
-    } else {
-        window.alert('不正解！');
-    }
-});
-document.getElementsByTagName('button')[2].addEventListener('click', () => {
-    if(correct === $button[2].textContent) {
-        window.alert('正解！');
-    } else {
-        window.alert('不正解！');
-    }
-});
-document.getElementsByTagName('button')[3].addEventListener('click', () => {
-    if(correct === $button[3].textContent) {
-        window.alert('正解！');
-    } else {
-        window.alert('不正解！');
-    }
-});
+}
+
+for(let i = 0; i < 4; i ++) {
+    $button[i].addEventListener('click', (e) => {
+        clickHandler(e);
+    });
+}
